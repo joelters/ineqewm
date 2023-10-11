b35749d9798175896f6ffe24c989c3aa57473d15
joelters <joelrt2310@gmail.com>
2023-10-11 19:09:37 +0200

    fix assign_quantiles

--- R/assign_quantiles.R
+++ R/assign_quantiles.R
@@ -5,8 +5,12 @@ assign_quantiles <- function(X,quants = 4){
   else {
     a <- 0
     for (i in 1:quants){
-      a <- a + quantile(X,i/quants)*(X >= quantile(X,i/quants - 1/quants) &
-                                   X <= quantile(X,i/quants))
+      if (i == 1){
+        a <- a + quantile(X,i/quants)*(X <= quantile(X,i/quants))
+      } else{
+        a <- a + quantile(X,i/quants)*(X > quantile(X,i/quants - 1/quants) &
+                                       X <= quantile(X,i/quants))
+      }
     }
     return(a)
   }
